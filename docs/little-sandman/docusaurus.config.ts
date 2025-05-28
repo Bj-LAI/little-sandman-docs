9242d71ce495e6115becd07018759bffcbc73a8e
Bj-LAI <bjornlai13@gmail.com>
2025-05-29 00:38:44 +0200

    Corrections config

--- docs/little-sandman/docusaurus.config.ts
+++ docs/little-sandman/docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Bj-LAI.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs',
+  baseUrl: '/little-sandman-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Bj-LAI', // Usually your GitHub org/user name.
+  projectName: 'little-sandman-docs', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
