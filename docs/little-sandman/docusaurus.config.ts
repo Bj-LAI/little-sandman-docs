1d7722eab2550b5c1b55789f20188f7d65467281
Bj-LAI <bjornlai13@gmail.com>
2025-05-29 10:51:01 +0200

    Correction config

--- docs/little-sandman/docusaurus.config.ts
+++ docs/little-sandman/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://bj-lai.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/little-sandman-docs/docs/little-sandman',
+  baseUrl: '/little-sandman-docs/',
   trailingSlash: false,
   deploymentBranch: 'gh-pages',
 
